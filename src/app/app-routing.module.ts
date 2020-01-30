5828b768edd123f41baf28a667090188b09f942a
Juan Manuel Mougan <juanmougan@gmail.com>
2020-01-29 23:29:52 -0300

    Navigation to product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-  }
+  { path: "", component: HomeComponent },
+  { path: "products/:productId", component: ProductDetailComponent }
 ];
 
 @NgModule({
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Product, ProductService } from "../shared/product.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'nga-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  selector: "auction-product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit() {
+    const prodId: number = parseInt(
+      // Extracts the parameter productId from the ActivatedRoute
+      this.route.snapshot.params["productId"]
+    );
+    this.product = this.productService.getProductById(prodId);
   }
-
 }
